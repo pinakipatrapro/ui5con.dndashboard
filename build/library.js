bdd5331c6c482d372505c0fa5e0636486db8df48
Pinaki patra <pinaki.patra.pro@gmail.com>
2022-06-19 12:01:33 +0000

    Build Updated

--- build/library.js
+++ build/library.js
@@ -1,4 +1,4 @@
 /*!
  * ${copyright}
  */
-sap.ui.define(["sap/ui/core/library","sap/ui/dom/includeStylesheet"],function(a){"use strict";var e=sap.ui.getCore().initLibrary({name:"ui5-cc-dndashboard",version:"1.0.0",dependencies:["sap.ui.core"],types:[],interfaces:[],controls:["ui5-cc-dndashboard.Dashboard","ui5-cc-dndashboard.DashboardTile","ui5-cc-dndashboard.DashboardChart","ui5-cc-dndashboard.FunnelChart"],elements:[],noLibraryCSS:false});return e});
+sap.ui.define(["sap/ui/core/library","sap/ui/dom/includeStylesheet"],function(a){"use strict";var e=sap.ui.getCore().initLibrary({name:"ui5-cc-dndashboard",version:"1.0.0",dependencies:["sap.ui.core"],types:[],interfaces:[],controls:["ui5-cc-dndashboard.Dashboard","ui5-cc-dndashboard.DashboardTile","ui5-cc-dndashboard.DashboardChart","ui5-cc-dndashboard.FunnelChart"],elements:[],noLibraryCSS:true});return e});
