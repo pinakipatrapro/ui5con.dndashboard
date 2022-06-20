0bfb9fdef76fbee03407ef841531d3aed1aa1846
pinakipatrapro <pinaki.patra.pro@gmail.com>
2022-06-21 01:41:34 +0530

    Test Changes

--- test/ui5-cc-dndashboard/qunit/Dashboard.qunit.js
+++ test/ui5-cc-dndashboard/qunit/Dashboard.qunit.js
@@ -7,6 +7,8 @@ sap.ui.define([
 ], function(qutils, createAndAppendDiv, library, Dashboard) {
 	"use strict";
 
+	// refer to library types
+	// var ExampleColor = library.ExampleColor;
 
 	// prepare DOM
 	createAndAppendDiv("uiArea1");
@@ -29,5 +31,33 @@ sap.ui.define([
 		});
 	})
 
+	// module for basic checks
+	QUnit.module("Basic Control Checks");
+
+	// some basic control checks
+	QUnit.test("Test get properties", function(assert) {
+		assert.expect(2);
+		var oDashboard = new Dashboard({
+		});
+		assert.equal(oDashboard.getHeight(), "80vh", "Check height equals '80vh'");
+		assert.equal(oDashboard.getEditable(), false, "Check default editable property");
+	});
+
+	// // some basic eventing check
+	// QUnit.test("Test click event", function(assert) {
+	// 	assert.expect(1);
+	// 	var oExample = new Example("example", {
+	// 		text: "Example",
+	// 		press: function() {
+	// 			assert.ok(true, "Event has been fired!")
+	// 		}
+	// 	}).placeAt("uiArea1");
+	// 	return new Promise(function(resolve, reject) {
+	// 		setTimeout(function() {
+	// 			qutils.triggerMouseEvent("example", "click", 1, 1);
+    //     resolve();
+	// 		}, 100);
+	// 	});
+	// });
 
 });
